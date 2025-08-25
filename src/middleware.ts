7e40c51e2e2d2791f6224224ddf88d9127aa461c
zeative <zaadevofc@gmail.com>
2025-08-26 03:46:02 +0700

    fix: wrong redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 export { default } from "next-auth/middleware"
 
-
 export const config = {
-  matcher: ["/admin/:path*", "/profile/:path*", "/orders/:path*", "/support/:path*", "/income/:path*", "/moderate/:path*", "/ekstra/:path*", "/designers/:path*"],
+  matcher: ["/((?!auth/login|not-approved).*)"],
 };
