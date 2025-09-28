ef8028dbc6d0c8b45e6f7486a3fcaecd22981702
zeative <zaadevofc@gmail.com>
2025-09-28 23:00:39 +0700

    remove: target blank

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -109,13 +109,13 @@
                     <ul class="flex gap-3 text-sm leading-normal">
                            @auth
                         <li>
-                             <a  href="{{ url('/dashboard') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                             <a  href="{{ url('/dashboard') }}" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                                  Dashboard
                              </a>
                          </li>
                     @else
                        <li>
-                            <a  href="{{ url('/dashboard/login') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                            <a  href="{{ url('/dashboard/login') }}" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                                 Login
                             </a>
                         </li>
