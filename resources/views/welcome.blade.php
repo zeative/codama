d18477b5ebe91032e84c5fa951602b7b87280aff
zeative <zaadevofc@gmail.com>
2025-09-28 22:17:54 +0700

    edit: nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -268,8 +268,8 @@
             </main>
         </div>
 
-        @if (Route::has('login'))
+        <!-- @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
-        @endif
+        @endif -->
     </body>
 </html>
