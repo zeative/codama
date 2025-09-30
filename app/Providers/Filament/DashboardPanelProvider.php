831d867660c7fdd0000db1ac644b679897afe5d3
zeative <zaadevofc@gmail.com>
2025-09-30 13:25:14 +0700

    add: seo

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -12,17 +12,31 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
+use Filament\Support\Facades\FilamentView;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\HtmlString;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class DashboardPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
+        FilamentView::registerRenderHook(
+            'panels::head.start',
+            fn(): string => new HtmlString('
+            <link rel="icon" type="image/png" href="/seo/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/svg+xml" href="/seo/favicon.svg" />
+            <link rel="shortcut icon" href="/seo/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/seo/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="Codama" />
+            <link rel="manifest" href="/seo/site.webmanifest" />
+        ')
+        );
+
         return $panel
             ->default()
             ->id('dashboard')
