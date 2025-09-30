bcc6e03074829c47d3c89a80d7be56bdf1359fb1
zeative <zaadevofc@gmail.com>
2025-09-30 17:04:22 +0700

    feat: modify icon

--- app/Filament/Resources/Galleries/GalleryResource.php
+++ app/Filament/Resources/Galleries/GalleryResource.php
@@ -65,4 +65,9 @@ class GalleryResource extends Resource
                 SoftDeletingScope::class,
             ]);
     }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
 }
